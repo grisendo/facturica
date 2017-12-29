08e5bed8cd1dad392a80a873f9d91cf9fbc335c1
Francisco J. Cruz Romanos <grisendo@gmail.com>
2017-12-30 00:01:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# facturica
+Generate invoices with custom templates through Cuentica API
+
+## 1. Install dependencies
+npm install
+
+## 2. Run the app
+npm start
+
+## 3. Configure
+Enter you Cuentica API key
+
+## 4. Customize invoice
+Edit file ./templates/default/template.ejs
+
+## 5. Enjoy!
---
+++ renderer.js
@@ -0,0 +1,117 @@
+const electron = require('electron');
+const ejs = require('ejs');
+const pdf = require('html-pdf');
+const config = require('electron-json-config');
+const smalltalk = require('smalltalk');
+const axios = require('axios');
+
+window.apiBaseUrl = 'https://api.cuentica.com/';
+window.apiToken = config.get('cuenticaAPIToken');
+window.ajaxConfig = {
+  headers: {'X-AUTH-TOKEN': window.apiToken}
+};
+
+window.askForAPIToken = function() {
+  smalltalk.prompt('API Token', 'Insert here your Cuentica API token', window.apiToken).then(
+    function(value) {
+      config.set('cuenticaAPIToken', value);
+      window.apiToken = config.get('cuenticaAPIToken');
+      window.ajaxConfig = {
+        headers: {'X-AUTH-TOKEN': window.apiToken}
+      };
+      if (!value) {
+        config.set('cuenticaAPIToken', '');
+        window.askForAPIToken();
+      } else {
+        axios.get(
+          window.apiBaseUrl + 'company',
+          window.ajaxConfig
+        ).then(
+          function(res) {
+            electron.remote.getCurrentWindow().reload();
+          },
+          function() {
+            electron.remote.dialog.showMessageBox(
+              {
+                type: 'error',
+                message: 'Invalid API token',
+                buttons: ['OK']
+              }
+            );
+            config.set('cuenticaAPIToken', '');
+            window.askForAPIToken();
+          }
+        );
+      }
+    },
+    function() {
+      if (!config.get('cuenticaAPIToken')) {
+        window.askForAPIToken();
+      }
+    }
+  );
+};
+
+electron.ipcRenderer.on('changeAPIKey', function() {
+  window.askForAPIToken();
+});
+
+if (!window.apiToken) {
+  window.askForAPIToken();
+}
+
+window.generatePDF = function(invoice) {
+  const templateData = {
+    invoice: invoice,
+    path: electron.remote.app.getAppPath(),
+    templatePath: electron.remote.app.getAppPath() + '/templates/default/'
+  };
+  const pdfConfig = {
+	  format: 'A4',
+	  orientation: 'portrait',
+	  type: 'pdf'
+  };
+  const dialog = electron.remote.dialog;
+  dialog.showSaveDialog(
+    {
+      title: 'Save invoice as...',
+      defaultPath: invoice.invoice_serie + '-' + invoice.invoice_number + '.pdf',
+      filters: [
+        {
+          name: 'Portable Document Format (*.pdf)',
+          extensions: ['pdf']
+        },
+        {
+          name: 'All files',
+          extensions: ['*']
+        }
+      ]
+    },
+    function(filepath) {
+      if (filepath) {
+        ejs.renderFile('./templates/default/template.ejs', templateData, {}, function(err, html) {
+          if (err) {
+            dialog.showMessageBox(
+              {
+                type: 'error',
+                message: 'Error creating invoice: ' + err,
+                buttons: ['OK']
+              }
+            );
+          } else {
+            pdf.create(html, pdfConfig).toFile(filepath, function(err, res){
+              dialog.showMessageBox(
+                {
+                  type: 'info',
+                  message: 'Invoice created successfully at ' + res.filename,
+                  buttons: ['OK']
+                }
+              );
+            });
+          }
+        });
+      }
+    }
+  );
+};
+
